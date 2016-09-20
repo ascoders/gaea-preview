a23d55507087ccaedc35f6d3398eed0f5ecb1bf4
huangziyi01 <huangziyi01@baidu.com>
2016-09-20 20:18:57 +0800

    add gaea

---
+++ gaea-preview/gaea-preview.component.tsx
@@ -0,0 +1,23 @@
+
+                    import * as React from 'react'
+                    import * as typings from './gaea-preview.type'
+                    import * as classNames from 'classnames'
+                    
+                    export default class GaeaPreview extends React.Component <typings.PropsDefine, typings.StateDefine> {
+                        static defaultProps: typings.PropsDefine = new typings.Props()
+                        public state: typings.StateDefine = new typings.State()
+                    
+                        render() {
+                            const classes = classNames({
+                                '_namespace': true,
+                                [this.props.className]: !!this.props.className
+                            })
+                        
+                            return (
+                                <div className={classes}>
+                                    盖亚-查看器
+                                </div>
+                            )
+                        }
+                    }
+                
---
+++ gaea-preview/gaea-preview.type.ts
@@ -0,0 +1,25 @@
+
+                    import * as React from 'react'
+
+                    export interface PropsDefine {
+
+                    }
+                    
+                    export class PropsGaea {
+                        gaeaName = '盖亚-查看器'
+                        gaeaIcon = 'square-o'
+                        gaeaUniqueKey = 'nt-editor-gaea-preview'
+                    }
+                    
+                    export class Props extends PropsGaea implements PropsDefine {
+                        
+                    }
+                    
+                    export interface StateDefine {
+                    
+                    }
+                    
+                    export class State implements StateDefine {
+                    
+                    }
+                

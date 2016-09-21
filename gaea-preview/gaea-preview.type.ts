f1666c5a8118851ca435c3105a289c13cc1f1f88
huangziyi01 <huangziyi01@baidu.com>
2016-09-21 11:27:13 +0800

    add gaea-model

--- gaea-preview/gaea-preview.type.ts
+++ gaea-preview/gaea-preview.type.ts
@@ -2,7 +2,24 @@ import * as React from 'react'
 import {TransparentlyPropsPropsDefine} from '../../../common/transparently-props/index'
 
 export interface PropsDefine extends TransparentlyPropsPropsDefine {
+    /**
+     * 自定义组件
+     */
+    customComponents?: any
 
+    /**
+     * 基础组件
+     * web 引用 fit-gaea/lib/web-components
+     * react-native 同时兼容 web 引用 fit-gaea/lib/native-components
+     */
+    baseComponents?: any
+
+    /**
+     * 页面信息
+     */
+    value?: {
+        [mapUniqueKey: string]: any
+    }
 }
 
 export class PropsGaea {

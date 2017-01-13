610fa39a01c1cf16490ba49a751ac0f1f038131e
huangziyi01 <huangziyi01@baidu.com>
2017-01-13 15:55:45 +0800

    发布组件

--- lib/index.d.ts
+++
@@ -1,5 +0,0 @@
-import  'gaea-model';
-import GaeaPreview from './gaea-preview/gaea-preview.component';
-import { PropsDefine as GaeaPreviewPropsDefine } from './gaea-preview/gaea-preview.type';
-export { GaeaPreview, GaeaPreviewPropsDefine };
-export default GaeaPreview;

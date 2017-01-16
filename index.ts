d81c23aa3e28eb3711418f217147e6cf4d8f82ba
huangziyi01 <huangziyi01@baidu.com>
2017-01-16 11:46:14 +0800

    gaea-editor增加时间窗口

--- index.ts
+++ index.ts
@@ -1,7 +1,13 @@
 import 'gaea-model'
+import * as LZString from 'lz-string'
 
 import GaeaPreview from './gaea-preview/gaea-preview.component'
 import {PropsDefine as GaeaPreviewPropsDefine} from './gaea-preview/gaea-preview.type'
 
 export {GaeaPreview, GaeaPreviewPropsDefine}
 export default GaeaPreview
+
+const LZDecode = LZString.decompressFromBase64
+const LZEncode = LZString.compressToBase64
+export {LZDecode as decode}
+export {LZEncode as encode}

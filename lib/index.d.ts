18c4d104cb934cb25e8bdf8d8d1a2044f2fd31d7
huangziyi01 <huangziyi01@baidu.com>
2017-01-22 14:39:28 +0800

    发布组件

--- lib/index.d.ts
+++
@@ -1,9 +0,0 @@
-import  'gaea-model';
-import GaeaPreview from './gaea-preview/gaea-preview.component';
-import { PropsDefine as GaeaPreviewPropsDefine } from './gaea-preview/gaea-preview.type';
-export { GaeaPreview, GaeaPreviewPropsDefine };
-export default GaeaPreview;
-declare const LZDecode: any;
-declare const LZEncode: any;
-export { LZDecode as decode };
-export { LZEncode as encode };
